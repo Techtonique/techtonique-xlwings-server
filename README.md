59337a443bf50d2cdeac65a9951a3b1b9bfdd3fe
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-09-19 03:37:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# xlwings server
+
+## License Key for non-commercial Purpose:
+
+To use xlwings PRO for free in a non-commercial context (as defined by the PolyForm Noncommercial License 1.0.0) use the following license key: noncommercial (Note that you need at least xlwings 0.26.0).
+
+## Activate a developer key
+
+To use xlwings PRO locally in your development environment, it’s easiest to run the following command:
+
+```bash
+xlwings license update -k YOUR_LICENSE_KEY
+```
---
+++ main.py
@@ -0,0 +1,100 @@
+import json 
+import os
+import requests
+import xlwings as xw
+
+from fastapi import Body, FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import PlainTextResponse
+
+from config import BASE_URL
+
+app = FastAPI()
+
+@app.post("/forecastingapicall")
+def api_call(token: str, 
+             filename: str, 
+             apiroute: str,            
+             data: dict = Body,
+             method: str = 'RidgeCV',
+             n_hidden_features: int = 5,
+             lags: int = 20,
+             type_pi: str = 'gaussian',
+             replications: int = 10,
+             h: int = 10):        
+
+    headers = {
+    'Authorization': 'Bearer ' + token,
+    }
+
+    params = {'method': method, 
+    'n_hidden_features': str(n_hidden_features),
+    'lags': str(lags),
+    'type_pi': str(type_pi),
+    'replications': str(replications),
+    'h': str(h)}
+
+    files = {
+        'file': open(filename, 'rb'),
+    }
+
+    response = requests.post(BASE_URL + apiroute, 
+        params=params, headers=headers, files=files)
+
+    res = response.json()
+
+    # transform strings to lists
+    mean = json.loads(res["mean"])
+    lower = json.loads(res["lower"])
+    upper = json.loads(res["upper"])
+
+    # Instantiate a Book object with the deserialized request body
+    with xw.Book(json=data) as book:
+        # Use xlwings as usual
+        sheet = book.sheets[0]
+        # Write the lists to Excel columns
+        sheet.range("A1").value = "Mean"
+        sheet.range("B1").value = "Lower"
+        sheet.range("C1").value = "Upper"
+
+        # Write the lists to Excel columns, one value per row
+        for i in range(len(mean)):
+            idx = i + 2            
+            sheet.range(f"A{idx}").value = mean[i]   # Write the 'mean' list in column A
+            sheet.range(f"B{idx}").value = lower[i]  # Write the 'lower' list in column B
+            sheet.range(f"C{idx}").value = upper[i]  # Write the 'upper' list in column C
+        # Pass the following back as the response
+        return book.json()
+
+@app.exception_handler(Exception)
+async def exception_handler(request, exception):
+    # This handles all exceptions, so you may want to make this more restrictive
+    return PlainTextResponse(
+        str(exception), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+    )
+
+
+# Office Scripts and custom functions in Excel on the web require CORS
+cors_app = CORSMiddleware(
+    app=app,
+    allow_origins="*",
+    allow_methods=["POST"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # Check if running on Heroku by checking for the existence of the 'PORT' environment variable
+    port = int(os.environ.get("PORT", 8001))
+
+    is_heroku = "PORT" in os.environ
+
+    if is_heroku:
+        # Running on Heroku
+        uvicorn.run("main:cors_app", host="0.0.0.0", port=port, reload=True)
+    else:
+        # Running locally
+        uvicorn.run("main:cors_app", host="0.0.0.0", port=8001, reload=True)
+
